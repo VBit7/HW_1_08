8c06ae0ddf3bfb66c1c967fdc1c5b91185493c05
Vitalii Bielimov <vbit2000@gmail.com>
2023-11-02 15:05:20 +0200

    Complete. All tests passed.

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
     # Реалізуйте тут домашнє завдання
-    return users
+
+    if not users: return {}
+
+    birthday_day = None
+    currend_date = date.today()
+    current_day = currend_date.strftime("%A")
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    days_of_week = days_of_week[days_of_week.index(current_day):] + days_of_week[:days_of_week.index(current_day)]
+    
+    next_week = currend_date + timedelta(days=7)
+    result = {day: [] for day in days_of_week}
+
+    for item in users:
+        next_birthday = item["birthday"].replace(year=currend_date.year)
+
+        if currend_date > next_birthday:
+            next_birthday = item["birthday"].replace(year=currend_date.year + 1)
+
+        if next_birthday <= next_week:            
+            birthday_day = item["birthday"].replace(year=currend_date.year).strftime("%A")
+
+            if birthday_day == "Saturday" or birthday_day == "Sunday":
+                birthday_day = "Monday"
+            
+            result[birthday_day].append(item["name"].split(" ")[0])
+
+    result_copy = result.copy()
+
+    for day, user in result_copy.items():
+        if not user:
+            result.pop(day)
+        
+    return result
 
 
 if __name__ == "__main__":
