33f6f035a2a66052d3459b37661e1320182b45b1
Vitalii Bielimov <vbit2000@gmail.com>
2023-11-02 17:07:32 +0200

    Bringing it up to the standard. Comments added

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
+"""
+Домашнє завдання. Python Core. Модуль 8
+"""
+
 from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
+    """
+    Виводить список колег,
+    яких потрібно привітати з днем народження на наступному тижні.
 
-    if not users: return {}
+    Параметри:
+    users (list): Список користувачів, кожен представлений словником
+    із ключами 'name' і 'birthday',
+    де 'name' - ім'я користувача, 'birthday' - день народження (datetime.date).
 
-    birthday_day = None
+    Повертає:
+    dict: Словник з днями тижня
+    ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+    та списками користувачів, яких потрібно привітати на цих днях.
+    Враховується наступний тиждень, включаючи поточний день.
+    Дні народження, які припадають на вихідні, переносяться на понеділок.
+
+    Приклад використання:
+    users = [
+        {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()},
+    ]
+    result = get_birthdays_per_week(users)
+
+    Результат буде у форматі {'Monday': ['Bill', 'Jan'], 'Wednesday': ['Kim']}
+    """
+
+    if not users:
+        return {}
+
     currend_date = date.today()
     current_day = currend_date.strftime("%A")
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-    days_of_week = days_of_week[days_of_week.index(current_day):] + days_of_week[:days_of_week.index(current_day)]
-    
+
+    # Переформатовуємо список днів тижня, починаючи з поточного дня
+    days_of_week = days_of_week[days_of_week.index(current_day):] + \
+        days_of_week[:days_of_week.index(current_day)]
+
     next_week = currend_date + timedelta(days=7)
     result = {day: [] for day in days_of_week}
 
     for item in users:
+
+        # Оновлюємо день народження на поточний рік
         next_birthday = item["birthday"].replace(year=currend_date.year)
 
+        # Якщо день народження вже минув у поточному році, то встановлюємо його на наступний рік
         if currend_date > next_birthday:
             next_birthday = item["birthday"].replace(year=currend_date.year + 1)
 
-        if next_birthday <= next_week:            
-            birthday_day = item["birthday"].replace(year=currend_date.year).strftime("%A")
+        # Перевіряємо, чи наступний день народження користувача знаходиться в межах наступного тижня
+        if next_birthday <= next_week:
+            birthday_day = next_birthday.strftime("%A")
 
-            if birthday_day == "Saturday" or birthday_day == "Sunday":
+            # Якщо день народження припадає на суботу або неділю, відкладаємо його на понеділок
+            if birthday_day in ("Saturday", "Sunday"):
                 birthday_day = "Monday"
-            
+
             result[birthday_day].append(item["name"].split(" ")[0])
 
     result_copy = result.copy()
 
+    # Якщо список порожній, видаляємо цей день зі словника результатів
     for day, user in result_copy.items():
         if not user:
             result.pop(day)
-        
+
     return result
 
 
